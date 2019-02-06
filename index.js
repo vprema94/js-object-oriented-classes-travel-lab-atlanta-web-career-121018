10d7b27d6a0e7ee68a8dfeca5d0825b75878d777
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-06 08:24:21 -0500

    progress

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+class Driver {
+    constructor(name, date) {
+        this.name = name
+        this.date = date
+    } 
 
+    startDate() {
+        let dateObj = new Date(this.date) 
+    } 
+
+    yearsExperienceFromBeginningOf () {
+        startYr = getFulYear(dateObj) 
+        
+    }
+}
