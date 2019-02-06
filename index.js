77e6d4a0d0fd0485d4ca1aa3e065727594cbe9e1
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-06 08:42:14 -0500

    progress

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 class Driver {
-    constructor(name, date) {
-        this.name = name
-        this.date = date
-    } 
-
-    startDate() {
-        let dateObj = new Date(this.date) 
-    } 
-
-    yearsExperienceFromBeginningOf () {
-        startYr = getFulYear(dateObj) 
-        
+    constructor(name,startDate) {
+      this.name = name;
+      this.startDate = new Date(startDate);
     }
+
+    yearsExperienceFromBeginningOf(year) {
+        let ttlYears = year - this.startDate.getFullYear();
+        return ttlYears;
+    }
+
+} 
+
+let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+];
+
+class Route {
+    constructor(beginningLocation, endingLocation) {
+      this.beginningLocation = beginningLocation;
+      this.endingLocation = endingLocation;
+    }
+
+    blocksTravelled() {
+        let vertical = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+        console.log(vertical)
+    }
+
 }
